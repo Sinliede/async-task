354168a9a8abdb554cc1b47f1ea4e839ca6fef81
zhaitianyu <zhaitianyu@5dgravity.com>
2019-05-07 16:03:27 +0800

    update test case params

--- src/test/java/src/com/sinliede/asynctask/test/Main.java
+++ src/test/java/src/com/sinliede/asynctask/test/Main.java
@@ -11,10 +11,10 @@ import com.sinliede.asynctask.task.Producer;
  */
 public class Main {
     public static void main(String[] args) {
-        AsyncTaskService<Integer> asyncTaskService = new AsyncTaskService<>(10000, 10, 20, 1000, new TestConsumer());
+        AsyncTaskService<Integer> asyncTaskService = new AsyncTaskService<>(2000, 10, 20, 1000, new TestConsumer());
         Producer<Integer> producer = new TestProducer();
         int i = 0;
-        for (; i < 11000; i++) {
+        for (; i < 2000; i++) {
             asyncTaskService.submitTask(producer);
             System.out.println("iterate : " + i);
         }
